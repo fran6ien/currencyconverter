55c8cf36e2f848b42ff50485690db5b270c1ddae
fogne fosso francien <gnefo2012@gmail.com>
2018-06-30 20:44:27 +0100

    sw update 4

--- sw.js
+++ sw.js
@@ -5,7 +5,18 @@ var precacheFiles = [
       './',
       './index.html',
       './style.css',
-      './app.js'
+      './app.js',
+      './manifest.json',
+      './images/chrome-favicon-16-16.png',
+      './images/chrome-extensionmanagementpage-48-48.png',
+      './images/chrome-installprocess-128-128.png',
+      './images/android-launchericon-48-48.png',
+      './images/android-launchericon-72-72.png',
+      './images/android-launchericon-96-96.png',
+      './images/android-launchericon-144-144.png',
+      './images/android-launchericon-192-192.png',
+      './images/android-launchericon-512-512.png',
+      
     ];
 
 //Install stage sets up the cache-array to configure pre-cache content
